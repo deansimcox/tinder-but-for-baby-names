ac09ee16ab9cd6d1dd82524bad886de3298b68f0
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-12-30 19:37:49 +0100

    improved mobile first UI

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,12 +13,12 @@ const Footer = () => (
   >
     <div className='mx-20 flex flex-col gap-8'>
       <div className='flex flex-col max-h-100px'>
-        <div className='mb-[50px] h-[2px] bg-white opacity-10' />
+        <div className='mb-[20px] sm:mb-[50px] h-[2px] bg-white opacity-10' />
         <div className='flex items-center justify-between flex-wrap gap-4'>
-          <h4 className='font-extrabold text-[24px] text-white'>
+          <h4 className='font-extrabold text-[14px] sm:text-[24px] text-white'>
             Victor Martinez
           </h4>
-          <p className='font-normal text-[14px] text-white opacity-80'>
+          <p className='font-normal text-[10px] sm:text-[14px] text-white opacity-80'>
             Copyright © 2021 - 2022 Victor L. Martinez. All rights reserved.
           </p>
         </div>
